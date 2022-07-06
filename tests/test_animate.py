1145baf8e1fcf89cbd501114cd94e5a04598546e
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-07-05 22:41:27 -0400

    tests: adding problematic imports

--- tests/test_animate.py
+++ tests/test_animate.py
@@ -1,5 +1,11 @@
 import pytest
 
+from pyvirtualdisplay import Display
+import os
+display = Display(visible=0, size=(1280, 1024))
+display.start()
+
+from mayavi import mlab
 
 def test_2D_animation_on_demographic_file(tmp_path):
 
